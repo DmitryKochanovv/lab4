4541283db7f16ba1b33cca0d1254d365f4567606
DmitryKochanovv <145797156+DmitryKochanovv@users.noreply.github.com>
2025-11-19 12:22:48 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 
-namespace LAB1_5
+namespace labor4
 {
     class Program
     {
@@ -10,21 +10,18 @@ namespace LAB1_5
             Console.OutputEncoding = System.Text.Encoding.UTF8;
 
             Console.WriteLine("== ЗАДАНИЕ 1 ==");
-            Tasks.Task1(new List<int> { 1, 2, 3, 2, 5, 4, 2, 7, 9 }, 2);
+            Tasks.Task1();
 
             Console.WriteLine("\n== ЗАДАНИЕ 2 ==");
-            LinkedList<int> ll = new LinkedList<int>(new int[] { 1, 2, 3, 4, 2, 5, 2, 6 });
-            Tasks.Task2(ll, 2);
+            Tasks.Task2();
 
             Console.WriteLine("\n== ЗАДАНИЕ 3 ==");
             Tasks.Task3();
 
             Console.WriteLine("\n== ЗАДАНИЕ 4 ==");
-            Tasks.Task4("text");
+            Tasks.Task4("text.txt");
 
-            Console.WriteLine("\n== ЗАДАНИЕ 5 ==");
-            Tasks.createInputFile("phones.txt");
-            Tasks.Task5("phones.txt");
+            
         }
     }
 }
