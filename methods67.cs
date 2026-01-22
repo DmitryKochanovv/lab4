bf697bf25cf67d8fbde72503b1dc3acbe061d9a4
DmitryKochanovv <145797156+DmitryKochanovv@users.noreply.github.com>
2026-01-22 07:40:12 +0500

    Add files via upload

---
+++ methods67.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace lab67
+{
+
+    //создаём класс отрезок,
+    public class LineSegment
+    {
+        // Поля
+        private double x;
+        private double y;
+
+        
+        // Свойства
+        public double X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        public double Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        // Конструктор
+        public LineSegment()
+        {
+            x = 0;
+            y = 1;
+        }
+
+        //конструктор с параметрами принимает коорд х у,создаёт объект
+        public LineSegment(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        //ToString()
+        public override string ToString()
+        {
+            return "[" + x + "; " + y + "]";
+        }
+
+        //попало ли в отрезок
+        public bool Contains(double value)
+        {
+            double min = x < y ? x : y;
+            double max = x > y ? x : y;
+
+            return value >= min && value <= max;
+        }
+        
+        //ПЕРЕГРУЗКИ операторов
+        //тернарные операторы
+        // ! — длина отрезка,если х больше у то x-y иначе y-x
+        public static double operator !(LineSegment seg)
+        {
+            return seg.x > seg.y ? seg.x - seg.y : seg.y - seg.x;
+        }
+
+        //++обе координаты на 1
+        public static LineSegment operator ++(LineSegment seg)
+        {
+            seg.x = seg.x + 1;
+            seg.y = seg.y + 1;
+            return seg;
+        }
+
+        //целая часть X
+        public static explicit operator int(LineSegment seg)
+        {
+            return (int)seg.x;
+        }
+
+        //координата Y
+        public static implicit operator double(LineSegment seg)
+        {
+            return seg.y;
+        }
+
+        //отрезок + d
+        public static LineSegment operator +(LineSegment seg, int d)
+        {
+            return new LineSegment(seg.x + d, seg.y + d);
+        }
+
+        // d + отрезок
+        public static LineSegment operator +(int d, LineSegment seg)
+        {
+            return new LineSegment(seg.x + d, seg.y + d);
+        }
+
+        //попадает ли число в отрезок
+        public static bool operator <(LineSegment seg, int value)
+        {
+            return seg.Contains(value);
+        }
+
+        //не попало в отрезок
+        public static bool operator >(LineSegment seg, int value)
+        {
+            return !seg.Contains(value);
+        }
+    }
+}
---
+++ program67.cs
@@ -0,0 +1,48 @@
+﻿using lab67;
+using System;
+
+namespace Lab6_7
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("=== ТЕСТ КЛАССА LineSegment ===");
+
+            Console.Write("Введите X: ");
+            double x = double.Parse(Console.ReadLine());
+
+            Console.Write("Введите Y: ");
+            double y = double.Parse(Console.ReadLine());
+
+            LineSegment seg = new LineSegment(x, y);
+
+            Console.WriteLine("Создан отрезок: " + seg);
+
+            Console.Write("Введите число для проверки Contains: ");
+            double v = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Попадает ли число? " + seg.Contains(v));
+
+            Console.WriteLine("Длина отрезка (!seg): " + !seg);
+
+            Console.WriteLine("++: " + (++seg));
+
+            Console.WriteLine("целое числоX: " + (int)seg);
+
+            double yValue = seg; 
+            Console.WriteLine("double Y: " + yValue);
+
+            Console.Write("Введите d для операции +: ");
+            int d = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("seg + d = " + (seg + d));
+            Console.WriteLine("d + seg = " + (d + seg));
+
+            Console.Write("Введите число для проверки оператора <: ");
+            int k = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("попадает в отрезок < k ? " + (seg < k));
+        }
+    }
+}
